63a8eb04f8a7c8fd9a815cba42c5f71ecb0747bc
cindyn65 <39638861+cindyn65@users.noreply.github.com>
2019-04-07 11:49:29 +0800

    Add files via upload
    model.ts
    manifest

---
+++ model.ts
@@ -0,0 +1,26 @@
+import { getRenderedText } from '@angular/core/src/render3';
+
+export interface Category {
+    categoryName: string, 
+}
+
+export interface IteminCategory{
+    itemsInCategory: string, 
+}
+
+export interface CharacterDetails{
+    picture: string,
+    name: string, 
+    height: number,
+    mass: number, 
+    hair_color: string, 
+    skin_color: string,
+    eye_color: string, 
+    birth_year: number, 
+    gender: string,
+    Homeword: string,
+    films: string,
+}
+
+
+
